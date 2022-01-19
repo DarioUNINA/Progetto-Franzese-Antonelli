7f8631e067029e8290959f7e35128671ca4c614e
renato1926 <re.antonelli@studenti.unina.it>
2022-01-19 21:57:44 +0100

    modifiche HomePage

--- CorsiDiFormazione/src/CorsiDiFormazione/HomePage.java
+++ CorsiDiFormazione/src/CorsiDiFormazione/HomePage.java
@@ -1,6 +1,7 @@
 package CorsiDiFormazione;
 
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -8,73 +9,93 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.GridLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.SpringLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class HomePage extends JFrame {
 
+
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField passwordtextField_;
+	private JTextField NomeUtenteField_1;
 
 	/**
 	 * Launch the application.
 	 */
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					HomePage frame = new HomePage();
 					frame.setVisible(true);
+					frame.getContentPane().setBackground(Color.	LIGHT_GRAY);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
-//ciao mammma
+
 	/**
 	 * Create the frame.
 	 */
 	public HomePage() {
+		setTitle("GESTIONE CORSI DI FORMAZIONE");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 451, 300);
+		setBounds(100, 100, 585, 372);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(181, 66, 86, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(181, 107, 86, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel NomeUtenteLabel = new JLabel("      Nome Utente:");
-		NomeUtenteLabel.setBounds(89, 69, 92, 14);
+		JLabel NomeUtenteLabel = new JLabel(" Nome Utente:");
+		NomeUtenteLabel.setBounds(128, 69, 105, 41);
 		contentPane.add(NomeUtenteLabel);
 		
-		JLabel PasswordLabel_ = new JLabel("            Password:");
-		PasswordLabel_.setBounds(89, 110, 92, 14);
+		JLabel PasswordLabel_ = new JLabel("      Password:");
+		PasswordLabel_.setBounds(128, 102, 105, 41);
 		contentPane.add(PasswordLabel_);
 		
-		JLabel TitoloLabel = new JLabel("      GESTIONE CORSI DI FORMAZIONE");
-		TitoloLabel.setBounds(112, 0, 208, 44);
-		contentPane.add(TitoloLabel);
-		
 		JButton AccediButton = new JButton("ACCEDI");
-		AccediButton.setBounds(178, 138, 89, 23);
+		AccediButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		AccediButton.setBounds(233, 143, 106, 32);
 		contentPane.add(AccediButton);
-		
-		JButton RegistrazioneButton = new JButton("REGISTRATI");
-		RegistrazioneButton.setBounds(10, 227, 106, 23);
-		contentPane.add(RegistrazioneButton);
+		AccediButton.setBackground(Color.WHITE);
 		
 		JButton PasswordDimenticataButton = new JButton("Hai dimenticato la Password?");
-		PasswordDimenticataButton.setBounds(112, 172, 208, 23);
+		PasswordDimenticataButton.setBounds(170, 195, 232, 20);
 		contentPane.add(PasswordDimenticataButton);
+		PasswordDimenticataButton.setBackground(Color.WHITE);
+		
+		JButton RegistrazioneButton = new JButton("REGISTRATI");
+		RegistrazioneButton.setBounds(10, 290, 105, 32);
+		contentPane.add(RegistrazioneButton);
+		RegistrazioneButton.setBackground(Color.RED);
+		
+		passwordtextField_ = new JTextField();
+		passwordtextField_.setColumns(10);
+		passwordtextField_.setBounds(223, 112, 127, 20);
+		contentPane.add(passwordtextField_);
+		
+		NomeUtenteField_1 = new JTextField();
+		NomeUtenteField_1.setColumns(10);
+		NomeUtenteField_1.setBounds(223, 79, 127, 20);
+		contentPane.add(NomeUtenteField_1);
 	}
 }

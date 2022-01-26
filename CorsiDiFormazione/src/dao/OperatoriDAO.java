63844b8ccdedc3c1108d04b00941a06431d25ba6
renato1926 <re.antonelli@studenti.unina.it>
2022-01-26 14:51:10 +0100

    update recupero pass

--- CorsiDiFormazione/src/dao/OperatoriDAO.java
+++ CorsiDiFormazione/src/dao/OperatoriDAO.java
@@ -67,7 +67,7 @@ public class OperatoriDAO {
 	public boolean CheckNome(Operatori op) {
 		
 		try {
-			ResultSet rs = statement.executeQuery("SELECT o.id_operatore FROM operatori o WHERE nome = '" + op.getNomeUtente() + "'");
+			ResultSet rs = statement.executeQuery("SELECT o.id_operatore FROM operatori o WHERE nome_utente = '" + op.getNomeUtente() + "'");
 			
 			return rs.next();
 		}catch(SQLException e) {

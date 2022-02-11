ac4874e48c5c6815ec9d5e3d6764be45ff1b97a0
renato1926 <re.antonelli@studenti.unina.it>
2022-02-11 16:27:28 +0100

    Sincronizzazione Page iscrizione corso e statistiche corso

---
+++ CorsiDiFormazione/src/gui/IscriviStudenteGestoreCorsoPage.java
@@ -0,0 +1,174 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
+import dto.Corsi;
+import dto.Operatori;
+import dto.Studenti;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JComboBox;
+import java.util.Vector;
+
+public class IscriviStudenteGestoreCorsoPage extends JFrame {
+
+	private Controller theController;
+	private Operatori operatore;
+	private Corsi corso;
+	private Vector<Studenti> studenti;
+	
+	private Component url;
+	private ImageIcon imageicon;
+	
+	private JPanel contentPane;
+	private JPanel aggiungiStudentiPanel;
+	private JLabel aggiungiStudenteAlCorsoLabel;
+	private JLabel corsoLabel;
+	private JLabel selezionareStudenteLabel;
+	private JButton indietroButton;
+	private JButton aggiungiButton;
+
+	private JComboBox<Studenti> studentiComboBox;
+	
+	final Color grigioChiaro;
+	
+	public IscriviStudenteGestoreCorsoPage(Controller cont, Operatori operatore, Corsi corso) {
+		setResizable(false);
+		
+		theController = cont;
+		this.operatore = operatore;
+		this.corso = corso;
+		studenti = theController.getStudentiDisponibili(operatore.getIdOperatore());
+		studentiComboBox = new JComboBox<Studenti>(studenti);
+		
+		grigioChiaro = new Color(233,233,233);
+		
+		imageicon = new ImageIcon("napule.png");
+		setIconImage(imageicon.getImage());
+		setTitle("GESTIONE CORSI DI FORMAZIONE");
+		
+		getContentPane().setBackground(Color.LIGHT_GRAY);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 584, 368);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.orange);
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		aggiungiStudentiPanel = new JPanel();
+		aggiungiStudentiPanel.setBackground(grigioChiaro);
+		aggiungiStudentiPanel.setBorder(new LineBorder(new Color(0, 0, 0), 3));
+		aggiungiStudentiPanel.setBounds(10, 11, 548, 307);
+		contentPane.add(aggiungiStudentiPanel);
+		aggiungiStudentiPanel.setLayout(null);
+		
+		aggiungiStudenteAlCorsoLabel = new JLabel("AGGIUNGI STUDENTE AL CORSO");
+		aggiungiStudenteAlCorsoLabel.setForeground(new Color(65, 105, 225));
+		aggiungiStudenteAlCorsoLabel.setFont(new Font("Arial", Font.BOLD, 22));
+		aggiungiStudenteAlCorsoLabel.setBackground(Color.WHITE);
+		aggiungiStudenteAlCorsoLabel.setBounds(94, 11, 367, 33);
+		aggiungiStudentiPanel.add(aggiungiStudenteAlCorsoLabel);
+		
+		corsoLabel = new JLabel("CORSO: " + corso.getNome());
+		corsoLabel.setFont(new Font("Arial", Font.BOLD, 15));
+		corsoLabel.setBounds(30, 111, 414, 14);
+		aggiungiStudentiPanel.add(corsoLabel);
+		
+		selezionareStudenteLabel = new JLabel("Selezionare Studente:");
+		selezionareStudenteLabel.setFont(new Font("Arial", Font.BOLD, 15));
+		selezionareStudenteLabel.setBounds(30, 136, 163, 23);
+		aggiungiStudentiPanel.add(selezionareStudenteLabel);
+		
+		
+		studentiComboBox.setBounds(203, 137, 163, 22);
+		aggiungiStudentiPanel.add(studentiComboBox);
+		
+		indietroButton = new JButton("INDIETRO");
+		indietroButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		indietroButton.setBackground(Color.WHITE);
+		indietroButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				GestoreCorsiPage gcp = new GestoreCorsiPage(theController, operatore);
+				setVisible(false);
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				indietroButton.setBackground(Color.ORANGE);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				indietroButton.setBackground(Color.WHITE);
+			}
+		});
+		indietroButton.setFont(new Font("Arial", Font.BOLD, 15));
+		indietroButton.setBounds(10, 273, 121, 23);
+		aggiungiStudentiPanel.add(indietroButton);
+		
+		aggiungiButton = new JButton("AGGIUGI");
+		aggiungiButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		aggiungiButton.setBackground(Color.WHITE);
+		aggiungiButton.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(java.awt.event.MouseEvent e) {
+				aggiungiButton.setBackground(Color.GREEN);
+			}
+
+			public void mouseExited(java.awt.event.MouseEvent e) {
+				aggiungiButton.setBackground(Color.WHITE);
+			}
+			@Override
+			public void mouseClicked(MouseEvent e) {	
+				
+				String state = theController.aggiungiStudenteCorso(studenti.get(studentiComboBox.getSelectedIndex()).getMatricola(), corso.getIdCorso());
+				
+				if(state.equals("0")) {
+					alertStudenteIscrittoCorrettamente();
+					
+				}else
+					alertErroreIscrizioneStudente(state);
+			}
+		});
+		aggiungiButton.setFont(new Font("Arial", Font.BOLD, 15));
+		aggiungiButton.setBounds(417, 274, 121, 23);
+		aggiungiStudentiPanel.add(aggiungiButton);
+		
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	public void alertStudenteIscrittoCorrettamente() {
+		JOptionPane.showMessageDialog(this, "Studente iscritto correttamente!","<CONFERMA>", JOptionPane.INFORMATION_MESSAGE);
+		GestoreCorsiPage gcp = new GestoreCorsiPage(theController, operatore);
+		setVisible(false);
+	}
+	
+	public void alertErroreIscrizioneStudente(String state) {
+		
+		if(state.equals("10009")) 
+			JOptionPane.showMessageDialog(this, "Non puoi iscirvere uno studente ad un corso terminato!","<ATTENZIONE>", JOptionPane.WARNING_MESSAGE);
+		else
+			if(state.equals("10006"))
+				JOptionPane.showMessageDialog(this, "Non e stato possibile effettuare l'iscrizione perche'e' stato raggiunto il numero massimo di partecipanti ammessi al corso","<ATTENZIONE>", JOptionPane.WARNING_MESSAGE);
+			else
+				JOptionPane.showMessageDialog(this, "Errore sconsciuto durante l'iscrizione al corso","<ATTENZIONE>", JOptionPane.WARNING_MESSAGE);
+	}
+	
+}
---
+++ CorsiDiFormazione/src/gui/StatisticheCorsoPage.java
@@ -0,0 +1,158 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
+import dto.Corsi;
+import dto.Operatori;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JList;
+import dto.Studenti;
+import java.util.Vector;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+
+public class StatisticheCorsoPage extends JFrame {
+	
+	private Controller theController;
+	private Operatori operatore;
+	private Corsi corso;
+	private Vector<Studenti> studenti;
+	
+	private Component url;
+	private ImageIcon imageicon;
+	private JPanel contentPane;
+	private JPanel sfondoPane;
+	private JPanel statistichePanel;
+	private JLabel statisticheCorsoLabel;
+	private JLabel nomeCorsoLabel;
+	private JLabel numeroMinPresenzeLezioneLabel;
+	private JLabel numeroMaxPresenzeLezioneLabel;
+	private JLabel percentualeRiempimentoLabel;
+	private JLabel studentiAmmessiLabel;
+	private JScrollPane studentiAmmessiScrollPane;
+	private JList<Studenti> studentiList;
+	private JButton indietroButton;
+	
+	final Color azzurro;
+	final Color azzurroChiaro;
+	final Color grigio;
+	final Color grigioChiaro;
+	
+	public StatisticheCorsoPage(Controller cont, Operatori operatore, Corsi corso) {
+		
+		theController = cont;
+		this.operatore = operatore;
+		this.corso = corso;
+		
+		studenti = theController.setAllStudentiAmmessi(corso.getIdCorso());
+		studentiList = new JList<Studenti>(studenti);
+		
+		azzurro = new Color(153,211,223);
+		azzurroChiaro = new Color(136,187,214);
+		grigio = new Color(205,205,205);
+		grigioChiaro = new Color(233,233,233);
+		
+		imageicon = new ImageIcon("napule.png");
+		setIconImage(imageicon.getImage());
+
+		setResizable(false);
+		
+		getContentPane().setBackground(grigio);
+		setTitle("GESTIONE CORSI DI FORMAZIONE");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 900, 600);
+		
+		sfondoPane = new JPanel();
+		sfondoPane.setBackground(azzurro);
+		sfondoPane.setBorder(new LineBorder(Color.BLACK));
+		setContentPane(sfondoPane);
+		sfondoPane.setLayout(null);
+		
+		statistichePanel = new JPanel();
+		statistichePanel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		statistichePanel.setBackground(grigioChiaro);
+		statistichePanel.setBounds(10, 11, 864, 539);
+		sfondoPane.add(statistichePanel);
+		statistichePanel.setLayout(null);
+		
+		statisticheCorsoLabel = new JLabel("STATISTICHE CORSO");
+		statisticheCorsoLabel.setBounds(315, 7, 234, 26);
+		statisticheCorsoLabel.setForeground(Color.BLACK);
+		statisticheCorsoLabel.setFont(new Font("Arial", Font.BOLD, 22));
+		statisticheCorsoLabel.setBackground(Color.WHITE);
+		statistichePanel.add(statisticheCorsoLabel);
+		
+		nomeCorsoLabel = new JLabel("Corso: " + corso.getNome().toUpperCase());
+		nomeCorsoLabel.setFont(new Font("Arial", Font.BOLD, 20));
+		nomeCorsoLabel.setBounds(60, 103, 412, 24);
+		statistichePanel.add(nomeCorsoLabel);
+		
+		numeroMinPresenzeLezioneLabel = new JLabel("Numero Min di presenze a Lezione:");
+		numeroMinPresenzeLezioneLabel.setFont(new Font("Arial", Font.BOLD, 20));
+		numeroMinPresenzeLezioneLabel.setBounds(60, 219, 347, 24);
+		statistichePanel.add(numeroMinPresenzeLezioneLabel);
+		
+		numeroMaxPresenzeLezioneLabel = new JLabel("Numero Max di presenze a Lezione:");
+		numeroMaxPresenzeLezioneLabel.setFont(new Font("Arial", Font.BOLD, 20));
+		numeroMaxPresenzeLezioneLabel.setBounds(60, 160, 372, 24);
+		statistichePanel.add(numeroMaxPresenzeLezioneLabel);
+		
+		percentualeRiempimentoLabel = new JLabel("Percentuale Riempimento Lezioni:");
+		percentualeRiempimentoLabel.setFont(new Font("Arial", Font.BOLD, 20));
+		percentualeRiempimentoLabel.setBounds(60, 280, 346, 24);
+		statistichePanel.add(percentualeRiempimentoLabel);
+		
+		studentiAmmessiLabel = new JLabel("Studenti Ammessi:");
+		studentiAmmessiLabel.setFont(new Font("Arial", Font.BOLD, 20));
+		studentiAmmessiLabel.setBounds(528, 160, 254, 24);
+		statistichePanel.add(studentiAmmessiLabel);
+		
+		studentiAmmessiScrollPane = new JScrollPane();
+		studentiAmmessiScrollPane.setBounds(528, 183, 254, 244);
+		statistichePanel.add(studentiAmmessiScrollPane);
+		
+		studentiAmmessiScrollPane.setViewportView(studentiList);
+		studentiList.setVisibleRowCount(10);
+		studentiList.setFont(new Font("Arial", Font.BOLD, 18));
+		studentiList.setBorder(new LineBorder(new Color(0, 0, 0)));
+		studentiList.setBackground(new Color(136, 187, 214));
+		
+		indietroButton = new JButton("INDIETRO");
+		indietroButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				GestoreCorsiPage glp = new GestoreCorsiPage(theController, operatore);
+				setVisible(false);
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				indietroButton.setBackground(Color.ORANGE);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				indietroButton.setBackground(Color.WHITE);
+			}
+		});
+		indietroButton.setBackground(Color.WHITE);
+		indietroButton.setFont(new Font("Arial", Font.BOLD, 15));
+		indietroButton.setBounds(10, 501, 127, 27);
+		statistichePanel.add(indietroButton);
+
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+	}
+}

e259f13ada17c6fe69aa1fe37aaa6c41923be39d
renato1926 <re.antonelli@studenti.unina.it>
2022-02-12 12:25:58 +0100

    ridesign statistiche

--- CorsiDiFormazione/src/gui/StatisticheCorsoPage.java
+++ CorsiDiFormazione/src/gui/StatisticheCorsoPage.java
@@ -32,30 +32,37 @@ public class StatisticheCorsoPage extends JFrame {
 	private Controller theController;
 	private Operatori operatore;
 	private Corsi corso;
-	private JList<String> studentiList;
 
 	
 	private Component url;
 	private ImageIcon imageicon;
 	private JPanel contentPane;
+	private JPanel infoPanel;
 	private JPanel sfondoPane;
 	private JPanel statistichePanel;
+	private JPanel studentiPanel;
 	private JLabel statisticheCorsoLabel;
 	private JLabel nomeCorsoLabel;
 	private JLabel numeroMinPresenzeLezioneLabel;
 	private JLabel numeroMaxPresenzeLezioneLabel;
+	private JLabel percentualeRiempimentoLabel;
 	private JLabel presenzeMedieLabel;
-	private JLabel studentiAmmessiLabel;
-	private JScrollPane studentiAmmessiScrollPane;
-	private JButton indietroButton;
 	private JProgressBar mediaRiempimentoProgressBar;
 	private Vector<String> studenti;
-
+	private JLabel studentiIscrittiLabel;
+	private JLabel studentiAmmessiLabel;
+	private JLabel numeroLezioniLabel;
+	private JButton indietroButton;
 	
 	final Color azzurro;
 	final Color azzurroChiaro;
 	final Color grigio;
 	final Color grigioChiaro;
+	private JList<String> studentiList;
+	private JScrollPane studenitAmmessiScrollPane;
+	private JLabel terminatoLabel;
+	private JLabel terminatoValueLabel;
+	private JLabel terminatoNoLabel;
 	
 	public StatisticheCorsoPage(Controller cont, Operatori operatore, Corsi corso) {
 		
@@ -63,7 +70,6 @@ public class StatisticheCorsoPage extends JFrame {
 		this.operatore = operatore;
 		this.corso = corso;
 		studenti = theController.setAllStudentiAmmessi(corso.getIdCorso());
-		
 		studentiList = new JList<String>(studenti);
 		
 		azzurro = new Color(153,211,223);
@@ -90,7 +96,7 @@ public class StatisticheCorsoPage extends JFrame {
 		statistichePanel = new JPanel();
 		statistichePanel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
 		statistichePanel.setBackground(grigioChiaro);
-		statistichePanel.setBounds(10, 11, 864, 539);
+		statistichePanel.setBounds(10, 11, 864, 44);
 		sfondoPane.add(statistichePanel);
 		statistichePanel.setLayout(null);
 		
@@ -101,87 +107,102 @@ public class StatisticheCorsoPage extends JFrame {
 		statisticheCorsoLabel.setBackground(Color.WHITE);
 		statistichePanel.add(statisticheCorsoLabel);
 		
+		infoPanel = new JPanel();
+		infoPanel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		infoPanel.setBackground(grigioChiaro);
+		infoPanel.setBounds(332, 66, 542, 484);
+		sfondoPane.add(infoPanel);
+		infoPanel.setLayout(null);
+		
 		nomeCorsoLabel = new JLabel("Corso: " + corso.getNome().toUpperCase());
+		nomeCorsoLabel.setBounds(10, 11, 522, 24);
+		infoPanel.add(nomeCorsoLabel);
 		nomeCorsoLabel.setFont(new Font("Arial", Font.BOLD, 20));
-		nomeCorsoLabel.setBounds(60, 103, 412, 24);
-		statistichePanel.add(nomeCorsoLabel);
 		
 		numeroMinPresenzeLezioneLabel = new JLabel("Numero Min di presenze a Lezione: " + theController.minimoPresenze(corso.getIdCorso()));
+		numeroMinPresenzeLezioneLabel.setBounds(10, 167, 522, 24);
+		infoPanel.add(numeroMinPresenzeLezioneLabel);
 		numeroMinPresenzeLezioneLabel.setFont(new Font("Arial", Font.BOLD, 20));
-		numeroMinPresenzeLezioneLabel.setBounds(22, 275, 450, 24);
-		statistichePanel.add(numeroMinPresenzeLezioneLabel);
 		
 		numeroMaxPresenzeLezioneLabel = new JLabel("Numero Max di presenze a Lezione: " + theController.massimoPresenze(corso.getIdCorso()));
 		numeroMaxPresenzeLezioneLabel.setFont(new Font("Arial", Font.BOLD, 20));
-		numeroMaxPresenzeLezioneLabel.setBounds(22, 240, 475, 24);
-		statistichePanel.add(numeroMaxPresenzeLezioneLabel);
+		numeroMaxPresenzeLezioneLabel.setBounds(10, 256, 522, 24);
+		infoPanel.add(numeroMaxPresenzeLezioneLabel);
 		
 		presenzeMedieLabel = new JLabel("Presenze medie per lezione: " + String.valueOf(theController.getPresenzeMedie(corso.getIdCorso())));
+		presenzeMedieLabel.setBounds(10, 337, 329, 24);
+		infoPanel.add(presenzeMedieLabel);
 		presenzeMedieLabel.setFont(new Font("Arial", Font.BOLD, 20));
-		presenzeMedieLabel.setBounds(22, 310, 346, 24);
-		statistichePanel.add(presenzeMedieLabel);
 		
-		studentiAmmessiLabel = new JLabel("Studenti Ammessi : " + theController.getNumeroStudentiAmmessi(corso.getIdOperatore()));
-		studentiAmmessiLabel.setFont(new Font("Arial", Font.BOLD, 20));
-		studentiAmmessiLabel.setBounds(22, 205, 254, 24);
-		statistichePanel.add(studentiAmmessiLabel);
+		percentualeRiempimentoLabel = new JLabel("Percentuale Riempimento Lezioni:");
+		percentualeRiempimentoLabel.setBounds(118, 418, 346, 24);
+		infoPanel.add(percentualeRiempimentoLabel);
+		percentualeRiempimentoLabel.setFont(new Font("Arial", Font.BOLD, 20));
 		
-		studentiAmmessiScrollPane = new JScrollPane();
-		studentiAmmessiScrollPane.setBounds(528, 183, 254, 244);
-		statistichePanel.add(studentiAmmessiScrollPane);
+		mediaRiempimentoProgressBar = new JProgressBar();
+		mediaRiempimentoProgressBar.setBounds(75, 453, 418, 20);		
+		mediaRiempimentoProgressBar.setBackground(Color.WHITE);
+		mediaRiempimentoProgressBar.setForeground(Color.BLACK);
+		mediaRiempimentoProgressBar.setFont(new Font("Microsoft YaHei UI", Font.BOLD | Font.ITALIC, 13));
+		mediaRiempimentoProgressBar.setStringPainted(true);
+		mediaRiempimentoProgressBar.setValue((int)theController.getPresenzeMedie(corso.getIdCorso())/studenti.size());
+		infoPanel.add(mediaRiempimentoProgressBar);
 		
-		studentiAmmessiScrollPane.setViewportView(studentiList);
+		terminatoLabel = new JLabel("Terminato: " );
+		terminatoLabel.setFont(new Font("Arial", Font.BOLD, 20));
+		terminatoLabel.setBounds(10, 86, 109, 24);
+		infoPanel.add(terminatoLabel);
+		
+		terminatoValueLabel = new JLabel("SI");
+		terminatoValueLabel.setForeground(Color.RED);
+		terminatoValueLabel.setFont(new Font("Arial", Font.BOLD, 20));
+		terminatoValueLabel.setBounds(140, 86, 30, 24);
+		infoPanel.add(terminatoValueLabel);
+		
+		if(corso.isTerminato()) {
+			terminatoValueLabel.setText("SI");
+			terminatoValueLabel.setForeground(Color.RED);
+		}else {
+			terminatoValueLabel.setText("NO");
+			terminatoValueLabel.setForeground(Color.GREEN);
+		}
+		
+		studentiPanel = new JPanel();
+		studentiPanel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		studentiPanel.setBounds(10, 66, 312, 484);
+		sfondoPane.add(studentiPanel);
+		studentiPanel.setLayout(null);
+		
+		studenitAmmessiScrollPane = new JScrollPane();
+		studenitAmmessiScrollPane.setBounds(10, 31, 292, 284);
+		studentiPanel.add(studenitAmmessiScrollPane);
+		
+		studenitAmmessiScrollPane.setViewportView(studentiList);
 		studentiList.setVisibleRowCount(10);
 		studentiList.setFont(new Font("Arial", Font.BOLD, 18));
 		studentiList.setBorder(new LineBorder(new Color(0, 0, 0)));
 		studentiList.setBackground(new Color(136, 187, 214));
 		
-		indietroButton = new JButton("INDIETRO");
-		indietroButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		indietroButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				GestoreCorsiPage glp = new GestoreCorsiPage(theController, operatore);
-				setVisible(false);
-			}
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				indietroButton.setBackground(Color.ORANGE);
-			}
-			@Override
-			public void mouseExited(MouseEvent e) {
-				indietroButton.setBackground(Color.WHITE);
-			}
-		});
-		indietroButton.setBackground(Color.WHITE);
-		indietroButton.setFont(new Font("Arial", Font.BOLD, 15));
-		indietroButton.setBounds(10, 501, 127, 27);
-		statistichePanel.add(indietroButton);
-		
-		JLabel percentualeRiempimentoLabel_1 = new JLabel("Percentuale Riempimento Lezioni:");
-		percentualeRiempimentoLabel_1.setFont(new Font("Arial", Font.BOLD, 20));
-		percentualeRiempimentoLabel_1.setBounds(22, 345, 346, 24);
-		statistichePanel.add(percentualeRiempimentoLabel_1);
-		
-		JLabel numeroLezioniLabel = new JLabel("Numero Lezioni: " + theController.getNumeroLezioni(corso.getIdCorso()));
-		numeroLezioniLabel.setFont(new Font("Arial", Font.BOLD, 20));
-		numeroLezioniLabel.setBounds(22, 168, 384, 26);
-		statistichePanel.add(numeroLezioniLabel);
-		
-		
-		mediaRiempimentoProgressBar = new JProgressBar();
-		mediaRiempimentoProgressBar.setBackground(Color.WHITE);
-		mediaRiempimentoProgressBar.setForeground(Color.BLACK);
-		mediaRiempimentoProgressBar.setFont(new Font("Microsoft YaHei UI", Font.BOLD | Font.ITALIC, 13));
-		mediaRiempimentoProgressBar.setStringPainted(true);
-		mediaRiempimentoProgressBar.setBounds(32, 380, 302, 20);
-		mediaRiempimentoProgressBar.setValue((int)theController.getPresenzeMedie(corso.getIdCorso())/corso.getMaxPartecipanti());
-		statistichePanel.add(mediaRiempimentoProgressBar);
-		
-		JLabel studentiIscrittiLabel = new JLabel("Studenti Iscritti (" + String.valueOf(studenti.size())  + ")");
+		studentiIscrittiLabel = new JLabel("Studenti Iscritti (" + studenti.size() + ")");
+		studentiIscrittiLabel.setBounds(66, 0, 189, 33);
+		studentiPanel.add(studentiIscrittiLabel);
 		studentiIscrittiLabel.setFont(new Font("Arial", Font.BOLD, 20));
-		studentiIscrittiLabel.setBounds(548, 158, 189, 14);
-		statistichePanel.add(studentiIscrittiLabel);
+	
+		studentiAmmessiLabel = new JLabel("Studenti Ammessi : " + theController.getNumeroStudentiAmmessi(corso.getIdCorso()));
+		studentiAmmessiLabel.setFont(new Font("Arial", Font.BOLD, 20));
+		studentiAmmessiLabel.setBounds(10, 326, 254, 24);
+		studentiPanel.add(studentiAmmessiLabel);
+		
+		numeroLezioniLabel = new JLabel("Numero Lezioni: " + theController.getNumeroLezioni(corso.getIdCorso()));
+		numeroLezioniLabel.setFont(new Font("Arial", Font.BOLD, 20));
+		numeroLezioniLabel.setBounds(10, 378, 384, 26);
+		studentiPanel.add(numeroLezioniLabel);
+		
+		indietroButton = new JButton("INDIETRO");
+		indietroButton.setBounds(10, 446, 292, 27);
+		studentiPanel.add(indietroButton);
+		indietroButton.setFont(new Font("Arial", Font.BOLD, 15));
+		indietroButton.setBackground(Color.WHITE);
 
 		setLocationRelativeTo(null);
 		setVisible(true);
